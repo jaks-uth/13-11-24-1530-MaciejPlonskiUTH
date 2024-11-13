9fcf0a246c2c4ec853d9441c145e3817805358d1
MaciejPlonskiUTH <167336989+MaciejPlonskiUTH@users.noreply.github.com>
2024-11-13 18:21:19 +0100

    Add files via upload

---
+++ src/main/java/pl/example/vehicles/Bike.java
@@ -0,0 +1,9 @@
+package pl.example.vehicles;
+
+public class Bike extends pl.example.vehicles.Vehicle {
+
+    public Bike(String name) {super(name);}
+
+
+    public int getMaxSpeed(){return 50;}
+}
